70e3a7572ad45a1f21d914b0dc119c538306c3da
Rishi Sharma <rishisharma132007@gmail.com>
2026-06-16 13:41:17 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,19 @@
+ const runCode = () => {
+    const students = [
+      { name: 'Aarav', marks: 78, passed: true, subject: 'Math' },
+      { name: 'Diya', marks: 42, passed: false, subject: 'Science' },
+      { name: 'Kabir', marks: 88, passed: true, subject: 'Math' },
+      { name: 'Meera', marks: 65, passed: true, subject: 'English' },
+      { name: 'Rohan', marks: 38, passed: false, subject: 'Science' },
+      { name: 'Isha', marks: 92, passed: true, subject: 'Math' },
+      { name: 'Vivaan', marks: 75, passed: true, subject: 'English' },
+      { name: 'Anaya', marks: 70, passed: true, subject: 'Science' }
+    ];
+
+  
+    const filteredStudents = students.filter(student => student.passed && student.marks >= 70);
+    console.log("1. Filtered Students:", filteredStudents);
+
+    const totalMarks = filteredStudents.reduce((sum, student) => sum + student.marks, 0);
+    console.log("2. Total Marks:", totalMarks);
+  }
